ff2bedd2a36a86e85ae4fd26156c907ca6aacc32
Vilela <47867188+Dibras@users.noreply.github.com>
2022-07-26 09:00:35 -0300

    Add files via upload

--- 3_Media_empresa.py
+++ 3_Media_empresa.py
@@ -1,6 +1,6 @@
 import json
 
-with open("3_dados_faturamento.json") as arc:
+with open("dados.json") as arc:
     dados = json.load(arc)
 
 cont = 0
@@ -10,20 +10,20 @@ maior = 0
 menor = 0
 
 for i in dados:
-    if i['Valor']!=0:
+    if i['valor']!=0:
 
-        if i['Valor']>maior or maior==0:
-            maior = i['Valor']
-        if i['Valor']<menor or menor==0:
-            menor = i['Valor']
+        if i['valor']>maior or maior==0:
+            maior = i['valor']
+        if i['valor']<menor or menor==0:
+            menor = i['valor']
 
-        soma_media += i['Valor'] 
+        soma_media += i['valor'] 
         cont += 1
 for i in dados:
-    if i['Valor']>soma_media/cont:
+    if i['valor']>soma_media/cont:
         cont2 +=1
 
-# print(maior, menor, soma_media, cont,soma_media/cont,cont2)
+print(maior, menor, soma_media, cont,soma_media/cont,cont2)
 print("O maior valor de faturamento no mes foi: R$",maior,)
 print("O menor valor de faturamento no mes foi: R$",menor)
 print(cont2,"dia(s) no mes teve/tiveram o faturamento maior que a média")
