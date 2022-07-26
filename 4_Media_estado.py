d36042049289eec9dc78289aa958edf8df654553
Vilela <47867188+Dibras@users.noreply.github.com>
2022-07-26 09:09:00 -0300

    Update 4_Media_estado.py

--- 4_Media_estado.py
+++ 4_Media_estado.py
@@ -3,11 +3,13 @@ sp = 67836.43
 rj = 36678.66
 mg = 29229.88
 es = 27165.48
-Outros = 19849.53
+outros = 19849.53
+
 soma=0
-soma = sp + rj + mg + es + Outros
+soma = sp + rj + mg + es + outros
+
 print("SP em relação porcentagem do total: %.2f" % (sp/soma*100),"%")
 print("RJ em relação porcentagem do total: %.2f" % (rj/soma*100),"%")
 print("MG em relação porcentagem do total: %.2f" % (mg/soma*100),"%")
 print("ES em relação porcentagem do total: %.2f" % (es/soma*100),"%")
-print("Outros em relação porcentagem do total: %.2f" % (Outros/soma*100),"%")
+print("Outros em relação porcentagem do total: %.2f" % (outros/soma*100),"%")
